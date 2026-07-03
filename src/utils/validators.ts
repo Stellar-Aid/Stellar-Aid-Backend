485747a60e1f2f7613d4f16c47b8bf16fa11fd70
Aniekan Victory <trovic101@gmail.com>
2026-07-03 13:26:24 +0100

    chore: fix minor linting warnings and strict typings pass 1

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -8,3 +8,4 @@ export const isPositiveAmount_7 = (n: number) => n > 0;
 export const isPositiveAmount_8 = (n: number) => n > 0;
 export const isPositiveAmount_9 = (n: number) => n > 0;
 export const isPositiveAmount_10 = (n: number) => n > 0;
+// Lint rule applied 1
