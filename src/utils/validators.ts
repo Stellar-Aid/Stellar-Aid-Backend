85972f96ee383bc01cc5ed9e6ea629daef53fa80
Aniekan Victory <trovic101@gmail.com>
2026-07-03 13:26:13 +0100

    refactor: separate numeric assertion rules for edge case 5

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -2,3 +2,4 @@ export const isValidAddress = (a: string) => a.startsWith('G');
 export const isPositiveAmount_2 = (n: number) => n > 0;
 export const isPositiveAmount_3 = (n: number) => n > 0;
 export const isPositiveAmount_4 = (n: number) => n > 0;
+export const isPositiveAmount_5 = (n: number) => n > 0;
