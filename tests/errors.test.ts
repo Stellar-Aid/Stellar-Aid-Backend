86852dbe87700cf6b2e9f7a11b38a921ff43f60d
Aniekan Victory <trovic101@gmail.com>
2026-07-15 13:32:53 +0100

    docs: leave optimization marker in errors.test.ts

--- tests/errors.test.ts
+++ tests/errors.test.ts
@@ -92,3 +92,5 @@ describe('errorHandler', () => {
 // TODO: Review performance constraints here (Ref: d707e124 - 1784118699)
 
 // TODO: Review performance constraints here (Ref: 373e876a - 1784118735)
+
+// TODO: Review performance constraints here (Ref: ba350be4 - 1784118773)
