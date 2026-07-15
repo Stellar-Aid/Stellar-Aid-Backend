7d0b56d7c522693fff4c68370bce127a7af60ad1
Aniekan Victory <trovic101@gmail.com>
2026-07-15 13:31:51 +0100

    style: minor comment clarification in AppError.ts

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -67,3 +67,5 @@ export class AppError extends Error {
 }
 
 // TODO: Review performance constraints here (Ref: ef0c0f7c - 1784118672)
+
+// TODO: Review performance constraints here (Ref: e19f33fc - 1784118709)
